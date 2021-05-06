886aaf4f129d0ee02e8a2ddcf41906b63d4d0574
rpetzandrian <rpetzandrian@gmail.com>
2021-05-06 20:37:59 +0700

    disabled form on load

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -7,7 +7,7 @@ import './styles/style2.css'
 import { Footer, Navbar } from '../../components';
 import { orderHis } from '../../Redux/Actions/order'
 import { useForm } from 'react-hook-form';
-import  moment  from 'moment'
+import moment from 'moment'
 
 
 export const Profile = () => {
@@ -16,15 +16,16 @@ export const Profile = () => {
   const formData = new FormData()
   const [activeTabs, setActiveTabs] = useState(1);
   const [photo, SetPhoto] = useState([])
+  const [readOnly, setReadOnly] = useState(true)
   const { data: userData } = useSelector((state) => state.UserLogin)
   const { data: user } = useSelector((state) => state.FetchUser)
-  const { data: order } = useSelector((state)=>state.GetOrderHis)
+  const { data: order } = useSelector((state) => state.GetOrderHis)
   const { error } = useSelector((state) => state.UpdateUser)
 
   const { register, handleSubmit, formState: { errors } } = useForm();
 
-  const formatdate = (date)=>{
-    let dateNow = moment(date,'YYYYMMDD').format('dddd, Do MMMM YYYY')
+  const formatdate = (date) => {
+    let dateNow = moment(date, 'YYYYMMDD').format('dddd, Do MMMM YYYY')
     return dateNow
   }
   useEffect(() => {
@@ -32,15 +33,17 @@ export const Profile = () => {
   }, [dispatch, userData, photo])
 
   // const onSubmit = data => console.log(data)
-  useEffect(()=>{
+  useEffect(() => {
     formData.append('photo', photo[0])
     dispatch(UpdateUser(userData.data, formData))
     console.log(formData)
-    
+
   }, [photo])
-  useEffect(()=>{
+
+  useEffect(() => {
     dispatch(orderHis(userData.data.id));
-  },[])
+  }, [])
+
   const onSubmit = (data) => {
     if (data.password !== '') {
       if (data.password !== data.confirm) {
@@ -53,11 +56,8 @@ export const Profile = () => {
       dispatch(UpdateUser(userData.data, data))
       dispatch(FetchUser(userData.data))
     }
+    setReadOnly(true)
   }
-
-  // const test = (e) => {
-  //   console.log(e)
-  // }
 
   return (
     <>
@@ -72,8 +72,8 @@ export const Profile = () => {
               </div>
               <div className='w-100 d-flex justify-content-center mt-3 photo-profile'>
                 <label>
-                    <input type="file" name="photo" accept="image" multiple onChange={(e) => SetPhoto(e.target.files)}/>
-                    <img width='130px' height='130px' className='rounded-circle' src={(user.photo)?`${process.env.REACT_APP_API_IMG_URL}${user.photo}`:`${process.env.PUBLIC_URL}/logo/no-photo.png`} alt='profile' />
+                  <input type="file" name="photo" accept="image" multiple onChange={(e) => SetPhoto(e.target.files)} />
+                  <img width='130px' height='130px' className='rounded-circle' src={(user.photo) ? `${process.env.REACT_APP_API_IMG_URL}${user.photo}` : `${process.env.PUBLIC_URL}/logo/no-photo.png`} alt='profile' />
                 </label>
               </div>
               <div className='w-100 d-flex justify-content-center mt-2 name'>
@@ -105,24 +105,24 @@ export const Profile = () => {
                 <form className='mt-3 mx-5 mb-5 row form-user' onSubmit={handleSubmit(onSubmit)}>
                   <div className='mt-4 col-12 col-md-6'>
                     <label htmlFor="inputFirstName" className="mb-3">First Name</label>
-                    <input type="text" defaultValue={user.first_name || register.first_name} className={`form-control py-3 px-4 form-input ${errors.first_name ? 'is-invalid' : ''}`} id="inputFirstName" {...register('first_name', { required: 'First name required' })} />
+                    <input type="text" defaultValue={user.first_name || register.first_name} className={`form-control py-3 px-4 form-input profile ${errors.first_name ? 'is-invalid' : ''}`} id="inputFirstName" {...register('first_name', { required: 'First name required' })} readOnly={readOnly} />
                     <small className='text-danger ms-3 mt-2'>{errors?.first_name?.message}</small>
                   </div>
                   <div className='mt-4 col-12 col-md-6'>
                     <label htmlFor="inputLastName" className="mb-3">Last Name</label>
-                    <input type="text" defaultValue={user.last_name || register.last_name} className={`form-control py-3 px-4 form-input ${errors.last_name ? 'is-invalid' : ''}`} id="inputLastName" {...register('last_name', { required: 'Last name required' })} />
+                    <input type="text" defaultValue={user.last_name || register.last_name} className={`form-control py-3 px-4 form-input profile ${errors.last_name ? 'is-invalid' : ''}`} id="inputLastName" {...register('last_name', { required: 'Last name required' })} readOnly={readOnly} />
                     <small className='text-danger ms-3 mt-2'>{errors?.last_name?.message}</small>
                   </div>
                   <div className='mt-4 col-12 col-md-6'>
                     <label htmlFor="inputEmail" className="mb-3">Email</label>
-                    <input type="email" defaultValue={user.email || register.email} className={`form-control py-3 px-4 form-input ${errors.email ? 'is-invalid' : ''}`} id="inputEmail" {...register('email', { required: 'Email required' })} />
+                    <input type="email" defaultValue={user.email || register.email} className={`form-control py-3 px-4 form-input profile ${errors.email ? 'is-invalid' : ''}`} id="inputEmail" {...register('email', { required: 'Email required' })} readOnly={readOnly} />
                     <small className='text-danger ms-3 mt-2'>{errors?.email?.message}</small>
                   </div>
                   <div className='mt-4 col-12 col-md-6'>
                     <label htmlFor="inputPhone" className="mb-3">Phone</label>
                     <div className="input-group">
                       <span className="input-group-text ps-4 group-text">+62</span>
-                      <input type="text" defaultValue={user.phone || register.phone} className={`form-control py-3 px-4 form-input-group ${errors.phone ? 'is-invalid' : ''}`} id="inputPhone" {...register('phone', { required: 'Phone required' })} />
+                      <input type="text" defaultValue={user.phone || register.phone} className={`form-control py-3 px-4 form-input-group profile ${errors.phone ? 'is-invalid' : ''}`} id="inputPhone" {...register('phone', { required: 'Phone required' })} readOnly={readOnly} />
                       <small className='text-danger ms-3 mt-2'>{errors?.password?.message}</small>
                     </div>
                   </div>
@@ -135,96 +135,94 @@ export const Profile = () => {
                 <form className='mt-3 mx-5 mb-5 row form-user'>
                   <div className='mt-4 col-12 col-md-6'>
                     <label htmlFor="inputPassword" className="mb-3">Password</label>
-                    <input type="password" defaultValue='' className="form-control py-3 px-4 form-input" id="inputPassword" {...register('password')} />
+                    <input type="password" defaultValue='' className="form-control py-3 px-4 form-input" id="inputPassword" {...register('password')} readOnly={readOnly} />
                   </div>
                   <div className='mt-4 col-12 col-md-6'>
                     <label htmlFor="inputConfirm" className="mb-3">Confirm Password</label>
-                    <input type="password" defaultValue='' className="form-control py-3 px-4 form-input" id="inputConfirm" {...register('confirm')} />
+                    <input type="password" defaultValue='' className="form-control py-3 px-4 form-input" id="inputConfirm" {...register('confirm')} readOnly={readOnly} />
                   </div>
                 </form>
               </div>
               <button className='mt-3 mx-5 mb-5 px-5 py-2 btn btn-rounded btn-update' onClick={handleSubmit(onSubmit)}>
                 Update Changes
               </button>
+              <button className='btn btn-rounded float-end px-5 py-2 mx-5 mt-3 btn-edit' onClick={() => setReadOnly(false)} >
+                Edit Profile
+              </button>
             </div>
 
             <div className={`${activeTabs === 2 ? 'd-block' : 'd-none'}`} >
-            {order.map((item)=>{
-                      return(
-              <div className='mt-3 py-3 card card-rounded order-history'>
-                <div className='d-flex justify-content-between px-5 order-head'>
-                  <div className=''>
-                    <small>{formatdate(`${item.order_date}`)}</small>
-                    <h5 className='mt-2'>{item.movie_title}</h5>
-                  </div>
-                  <img className='logo-cinema' src={`${process.env.REACT_APP_API_IMG_URL}${item.logo}`} alt='cinema' />
-                </div>
-                <hr className='mx-5 mt-4' />
-
-                <div id={`collapseOne${item.id}`} className="mx-5 accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-                  <div className="accordion-body">
-                     
-                      <img src={process.env.PUBLIC_URL + '/svg/logo.svg'} alt="logo tickitz" />
-                      <div className='row'>
-                        <div className='col-12 cols-sm-12 col-md-6'>
-                          <ul class="list-group list-group-flush">
-                            <li class="list-group-item">
-                              <h5 className='fw-bold'>Title : </h5>
-                              <small>{item.movie_title}</small>
-                            </li>
-                            <li class="list-group-item">
-                              <h5 className='fw-bold'>Date : </h5>
-                              <small>{moment(`${item.start_date}`,'YYYYMMDD').format('dddd, Do MMMM YYYY - hh:mma')}</small>
-                            </li>
-                          </ul>
-                        </div>
-                        <div className='col-12 col-sm-12 col-md-6'>
-                          <ul class="list-group list-group-flush">
-                            <li class="list-group-item">
-                              <h5 className='fw-bold'>Seat : </h5>
-                              <small>{item.seat.length} Pieces</small>
-                            </li>
-                            <li class="list-group-item">
-                              <h5 className='fw-bold'>Number Seat : </h5>
-                              <small>{`${item.seat}`}</small>
-                            </li>
-                          </ul>
-                        </div>
-                        <div className='col-12 card-body'>
-                        <ul class="list-group list-group-flush">
-                          <li class='list-group-item'>
-                          <div className="d-flex justify-content-between">
-                            <h5 className='fw-bold'>Total :</h5>
-                            <h5>Rp. {item.total_payment}</h5>
+              {order.map((item) => {
+                return (
+                  <div className='mt-3 py-3 card card-rounded order-history'>
+                    <div className='d-flex justify-content-between px-5 order-head'>
+                      <div className=''>
+                        <small>{formatdate(`${item.order_date}`)}</small>
+                        <h5 className='mt-2'>{item.movie_title}</h5>
+                      </div>
+                      <img className='logo-cinema' src={`${process.env.REACT_APP_API_IMG_URL}${item.logo}`} alt='cinema' />
+                    </div>
+                    <hr className='mx-5 mt-4' />
+                    <div id={`collapseOne${item.id}`} className="mx-5 accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                      <div className="accordion-body">
+                        <img src={process.env.PUBLIC_URL + '/svg/logo.svg'} alt="logo tickitz" />
+                        <div className='row'>
+                          <div className='col-12 cols-sm-12 col-md-6'>
+                            <ul class="list-group list-group-flush">
+                              <li class="list-group-item">
+                                <h5 className='fw-bold'>Title : </h5>
+                                <small>{item.movie_title}</small>
+                              </li>
+                              <li class="list-group-item">
+                                <h5 className='fw-bold'>Date : </h5>
+                                <small>{moment(`${item.start_date}`, 'YYYYMMDD').format('dddd, Do MMMM YYYY - hh:mma')}</small>
+                              </li>
+                            </ul>
                           </div>
-                          </li>
-                        </ul>
+                          <div className='col-12 col-sm-12 col-md-6'>
+                            <ul class="list-group list-group-flush">
+                              <li class="list-group-item">
+                                <h5 className='fw-bold'>Seat : </h5>
+                                <small>{item.seat.length} Pieces</small>
+                              </li>
+                              <li class="list-group-item">
+                                <h5 className='fw-bold'>Number Seat : </h5>
+                                <small>{`${item.seat}`}</small>
+                              </li>
+                            </ul>
+                          </div>
+                          <div className='col-12 card-body'>
+                            <ul class="list-group list-group-flush">
+                              <li class='list-group-item'>
+                                <div className="d-flex justify-content-between">
+                                  <h5 className='fw-bold'>Total :</h5>
+                                  <h5>Rp. {item.total_payment}</h5>
+                                </div>
+                              </li>
+                            </ul>
+                          </div>
                         </div>
                       </div>
-
-                   
-                  </div>  
-                </div>
-
-                <div className='mx-5 my-3 d-flex justify-content-between align-items-center'>
-                  {(item.status === 'checkout') ?(
-                  <div className='py-2 px-2 px-md-5 text-center status-checkout' onClick={()=>history.push(`/payment/${item.id.split('-')[1]}`)}>
-                    Checkout
-                  </div>
-                  ):(
-                    <div className='py-2 px-2 px-md-5 text-center status-active' onClick={()=>history.push(`/ticket-result/${item.id.split('-')[1]}`)}>
-                    Active
-                  </div>
-                  )
-                  }
-                  <p className='text-muted d-flex accor-toggle' data-bs-toggle="collapse" data-bs-target={`#collapseOne${item.id}`} aria-expanded="true" aria-controls={`collapseOne${item.id}`}>
-                    Show Details
+                    </div>
+                    <div className='mx-5 my-3 d-flex justify-content-between align-items-center'>
+                      {(item.status === 'checkout') ? (
+                        <div className='py-2 px-2 px-md-5 text-center status-checkout' onClick={() => history.push(`/payment/${item.id.split('-')[1]}`)}>
+                          Checkout
+                        </div>
+                      ) : (
+                        <div className='py-2 px-2 px-md-5 text-center status-active' onClick={() => history.push(`/ticket-result/${item.id.split('-')[1]}`)}>
+                          Active
+                        </div>
+                      )
+                      }
+                      <p className='text-muted d-flex accor-toggle' data-bs-toggle="collapse" data-bs-target={`#collapseOne${item.id}`} aria-expanded="true" aria-controls={`collapseOne${item.id}`}>
+                        Show Details
                     <img className='d-none d-sm-block mx-3' src={process.env.PUBLIC_URL + '/svg/arrow-down.svg'} alt='arrow' />
-                  </p>
-                </div>
-              </div>
-              )
-                    })}
+                      </p>
+                    </div>
+                  </div>
+                )
+              })}
             </div>
           </div>
         </div>
